c8dcca694e710fc41b92a4b0aea7ead4f15acd9f
nhck3001 <khainguyenngoc11@gmail.com>
2025-04-26 21:01:15 -0400

    aa

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from io import BytesIO
 import base64
 from sqlalchemy import create_engine
 import pandas as pd
-import pyodbc
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib
